19bcc0bbb89b9c0d4f642abdeb664b5aa9ee16eb
Knight-Devil <adikulkarni2003@gmail.com>
2024-09-20 23:16:59 +0530

    Basics of cpp

---
+++ Basic.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    // The library <bits/stdc++.h> includes all the libraries required for CP....
+    cout << sqrt(36);
+
+    return 0;
+}
